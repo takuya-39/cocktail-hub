3d23788de56d1275b044949401d7a19898f002ad
takuya_33 <yatto733@gmail.com>
2020-06-01 01:19:20 +0900

    [add]RSpec導入
    RSpecの導入を行った

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,13 @@ module CocktailHub
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false
+    end
   end
 end
