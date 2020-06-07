ca47ffdf0227fc766da8536b5f361736a456c38e
takuya_33 <yatto733@gmail.com>
2020-06-08 05:49:33 +0900

    [update]rubocop更新
    .rubocop.ymlの内容を更新し、静的解析後コード修正などを行った

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,38 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   let(:post) { FactoryBot.create(:post) }
 
-  describe "index" do
-    it "正常にアクセスできること" do
+  describe 'index' do
+    it '正常にアクセスできること' do
       get root_path
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "show" do
-    it "正常にアクセスできること" do
+  describe 'show' do
+    it '正常にアクセスできること' do
       get post_path(post)
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "new" do
-    it "正常にアクセスできること" do
+  describe 'new' do
+    it '正常にアクセスできること' do
       get new_post_path
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "edit" do
-    it "正常にアクセスできること" do
+  describe 'edit' do
+    it '正常にアクセスできること' do
       get edit_post_path(post)
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
     end
   end
-
 end
