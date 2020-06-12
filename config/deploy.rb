1b4b50f2d5f7db332203b9b1365079c910ccba70
takuya_33 <yatto733@gmail.com>
2020-06-12 22:38:51 +0900

    [add]deploy.rb修正
    デプロイ時にAWS関連のエラーが出る為、試しにdeploy.rbの追記を行った

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,14 @@ set :keep_releases, 5
 
 set :log_level, :debug
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_REGION: ENV["AWS_REGION"],
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 namespace :deploy do
   desc 'Restart application'
   task :restart do
