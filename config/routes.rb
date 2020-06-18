39a39a8781b384faea8aad32250ac957642344c8
takuya_33 <yatto733@gmail.com>
2020-06-19 07:55:21 +0900

    [add]devise導入
    deviseを導入し、UsersControllerやUserModelの作成を行った

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 
   resources :posts
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'signup', :to => 'users/registrations#new'
+    get 'login', :to => 'users/sessions#new'
+    get 'logout', :to => 'users/sessions#destroy'
+  end
 end
