07237c372649e9c200dcf440b19fb625900a0722
takuya_33 <yatto733@gmail.com>
2020-06-22 04:33:48 +0900

    [update]authenticate_user!の設定修正
    投稿一覧ページのみログインしなくてもアクセスできるように修正を行った

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :new, :create, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
