9eadd6b0634b7df69948a66de0f923faee826b2b
takuya_33 <yatto733@gmail.com>
2020-11-22 09:51:26 +0900

    [update]新規ユーザー作成ページのレイアウト修正
    新規ユーザー作成ページのレイアウトの修正、その他関連コードの修正を行った

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # デバイスのエラーメッセージ出力メソッド
+  def devise_error_messages
+    return '' if resource.errors.empty?
+
+    html = ''
+    # エラーメッセージ用のHTMLを生成
+    resource.errors.full_messages.each do |msg|
+      html += <<-RUBY
+        <div class="error_field alert alert-danger" role="alert">
+          <p class="error_msg">#{ msg }</p>
+        </div>
+      RUBY
+    end
+    html.html_safe
+  end
 end
