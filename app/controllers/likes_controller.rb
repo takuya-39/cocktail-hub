1f49b3164c253fa15e7d3d822b4619f5e2e29895
takuya_33 <yatto733@gmail.com>
2020-11-30 08:14:45 +0900

    [update]いいねボタンの修正
    いいねボタン部分のパーシャル切り出し、Ajax化を行った

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,20 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_post
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_back(fallback_location: @post)
+    @likes = Like.where(post_id: params[:post_id])
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: @post)
+    @likes = Like.where(post_id: params[:post_id])
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
