a70b2eb8ed3ba2ebfb47733dea97e4016a11dac5
takuya_33 <yatto733@gmail.com>
2020-12-09 01:50:52 +0900

    [update]users_signup_spec.rbの修正
    users_signup_spec.rbの修正を行った

--- spec/system/users_signup_spec.rb
+++ spec/system/users_signup_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe 'Signup', type: :system, js: true do
 
   it 'ユーザーがサインアップできること' do
     visit root_path
-
     find('.nav-icon-btn').click
     find('.signup').click
+    expect(current_path).to eq signup_path
 
     perform_enqueued_jobs do
       expect do
         fill_in 'UserName', with: 'サインアップユーザー'
-        fill_in 'Email', with: 'test@example.com'
+        fill_in 'Email', with: 'signup@example.com'
         fill_in 'Password', with: 'password'
         fill_in 'PasswordConfirmation', with: 'password'
         fill_in 'Profile', with: 'サインアップユーザーです。'
         attach_file 'UserImage', "#{ Rails.root }/spec/support/assets/sample_user_image_cat.jpg"
         click_button '新規ユーザー登録をする'
       end.to change(User, :count).by(1)
-
-      expect(page).to have_content 'サインアップユーザー'
     end
+    expect(page).to have_content 'サインアップユーザー'
+    expect(page).to have_content 'サインアップユーザーです。'
   end
 end
